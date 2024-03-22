038d739c30edc861566234f3e78aa86ae51165b6
Raphaël Oculi <raphael.oculi@etu.minesparis.psl.eu>
2024-03-22 21:34:42 +0100

    travail calculette

--- subject/script.js
+++ subject/script.js
@@ -1,5 +1,80 @@
-// NOTE: 
-// This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+const display = calculator.querySelector('.calculator__display');
+let ancien = null;
+let opérateur = null;
+let opération = false;
 
-// # START EDITING YOUR JAVASCRIPT HERE
-// ===============
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target;
+    const keyValue = key.textContent;
+
+    if (key.dataset.action === 'clear') {
+      display.textContent = '0';
+      ancien = null;
+      opérateur = null;
+      opération = false;
+      return;
+    }
+
+    if (!isNaN(keyValue)) {
+      if (display.textContent === '0' || opération) {
+        display.textContent = keyValue;
+        opération = false;
+      } else {
+        display.textContent += keyValue;
+      }
+      return;
+    }
+
+    if (key.classList.contains('key--operator')) {
+      const currentValue = parseFloat(display.textContent);
+      if (opérateur && !opération) {
+        calculate(currentValue);
+      } else {
+        ancien = currentValue;
+      }
+      opérateur = keyValue;
+      opération = true;
+      return;
+    }
+
+    if (key.dataset.action === 'calculate') {
+      const currentValue = parseFloat(display.textContent);
+      if (opérateur && !opération) {
+        calculate(currentValue);
+        opérateur = null;
+      } else {
+        ancien = currentValue;
+      }
+      return;
+    }
+  }
+});
+
+function calculate(terme) {
+  let résultat = 0;
+  switch (opérateur) {
+    case '+':
+      résultat = ancien + terme;
+      break;
+    case '-':
+      résultat = ancien - terme;
+      break;
+    case '×':
+      résultat = ancien * terme;
+      break;
+    case '÷':
+      if (terme === 0) {
+        display.textContent = 'Erreur';
+        return;
+      }
+      résultat = ancien / terme;
+      break;
+  }
+  display.textContent = résultat;
+  ancien = résultat;
+  opération = true;
+}
+
